77dbf143d53212d951ca7f4b68132bb1f775de23
LegoMaster616 <andrew.afflitto@gmail.com>
2014-04-04 23:35:02 -0400

    Removed call to tile.value
    tile is null in tileMatchesAvailable()

--- js/game_manager.js
+++ js/game_manager.js
@@ -270,7 +270,7 @@ GameManager.prototype.tileMatchesAvailable = function () {
 
           var other  = self.grid.cellContent(cell);
 
-          if (other && (Math.abs(other.value) === Math.abs(tile.value)) || other.value === 0 || tile.value == 0) {
+          if (other && (Math.abs(other.value) === Math.abs(tile.value)) || other.value === 0) {
             return true; // These two tiles can be merged
           }
         }
